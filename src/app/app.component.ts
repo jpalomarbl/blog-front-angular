dcacb44ec69d2ba49c260d0e2421fc8ddccb6ccf
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-07 10:28:37 +0100

    Added message list and form imports to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MessageFormComponent } from './Components/message-form/message-form.component';
+import { MessageListComponent } from './Components/message-list/message-list.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MessageFormComponent, MessageListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
