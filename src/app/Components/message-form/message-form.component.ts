094d3870b8df0e2fe80daef2a0d5e98a3c770db9
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-07 10:27:31 +0100

    Created message list and form components

---
+++ src/app/Components/message-form/message-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-form.component.html',
+  styleUrl: './message-form.component.css'
+})
+export class MessageFormComponent {
+
+}
