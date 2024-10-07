6d1c9d94b124af2e52f2abddb3be67996d8409ad
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-10-07 10:28:53 +0100

    Added routes to new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MessageListComponent } from './Components/message-list/message-list.component';
+import { MessageFormComponent } from './Components/message-form/message-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MessageListComponent
+    },
+    {
+        path: '/:id',
+        component: MessageFormComponent
+    }
+];
